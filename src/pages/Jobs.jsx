0bd47e11ec1aea40cea66ded39e56560dde582fc
Rushikesh Manjre <144887746+rushikesh221004@users.noreply.github.com>
2025-05-29 15:33:19 +0530

    all job section creted

--- src/pages/Jobs.jsx
+++ src/pages/Jobs.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import FindDreamJob from '../components/FindDreamJob'
+import AllJobs from '../components/AllJobs'
 
 const Jobs = () => {
   return (
     <div className="min-h-screen pb-20 w-full">
         <div className="xl:max-w-[1250px] flex flex-col items-center mx-auto">
             <FindDreamJob/>
+            <AllJobs/>
         </div>
     </div>
   )
